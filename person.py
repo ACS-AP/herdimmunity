f907a41bce1e258c0c27a8608ebb134228017c1f
ArnavPag <arnav.pagidyala@students.dominican.edu>
2021-12-04 17:58:37 -0800

    updated person, virus and simulation

--- person.py
+++ person.py
@@ -14,11 +14,7 @@ class Person(object):
         self.infection = None  # Virus object or None
 
     def did_survive_infection(self):
-        ''' Generate a random number and compare to virus's mortality_rate.
-        If random number is smaller, person dies from the disease.
-        If Person survives, they become vaccinated and they have no infection.
-        Return a boolean value indicating whether they survived the infection.
-        '''
+        
         chance_infection = random.random()
         if chance_infection > self.infection.mortality_rate:
             self.is_vaccinated = True
@@ -63,24 +59,19 @@ def test_sick_person_instantiation():
 
 
 def test_did_survive_infection():
-    # TODO: Create a Virus object to give a Person object an infection
+    
     virus = Virus("Dysentery", 0.7, 0.2)
-    # TODO: Create a Person object and give them the virus infection
+
     person = Person(4, False, virus)
 
-    # Resolve whether the Person survives the infection or not
     survived = person.did_survive_infection()
-    # Check if the Person survived or not
+    
     if survived:
         assert person.is_alive is True
-        # TODO: Write your own assert statements that test
-        # the values of each attribute for a Person who survived
-        # assert ...
+        assert person.is_vaccinated is True
     else:
         assert person.is_alive is False
-        # TODO: Write your own assert statements that test
-        # the values of each attribute for a Person who did not survive
-        # assert ...
+        
         pass
 
 my_virus = Virus("covid", .4, .2)
