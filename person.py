bd7181ba2b2231af79793417c35f9771fbe927e7
ArnavPag <arnav.pagidyala@students.dominican.edu>
2021-12-09 12:26:31 -0800

    updated person.py

--- person.py
+++ person.py
@@ -1,79 +1,84 @@
 import random
-random.seed(42)
 from virus import Virus
 
+
+
 class Person(object):
-    def __init__(self, _id, is_vaccinated, infection=None):
-        ''' 
-        If person is chosen to be infected when the population is created, the simulation
-        should instantiate a Virus object and set it as the value
-        self.infection. Otherwise, self.infection should be None.
-        '''
-        self._id = _id # int
-        self.is_alive = True  # bool
-        self.is_vaccinated = is_vaccinated  # bool
-        self.infection = infection  # Virus object or None
+
+    def __init__(self, _id, is_vaccinated, virus=None):
+        self._id = _id  
+        self.is_alive = True 
+        self.natural_immunity = False 
+        self.is_vaccinated = is_vaccinated  
+        self.virus = virus  
 
     def did_survive_infection(self):
         if self.virus: 
-            died = random.random()
-            if died < self.virus.died:
-                print(f'{self._id} has Died :(')
+            random_mortality = random.random()
+            if random_mortality < self.virus.mortality_rate:
+                print(f'{self._id} died')
                 self.is_alive = False
                 return False
             else:
+                self.natural_immunity = True
                 self.virus = None
                 return True
-
-# -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
-# Testing person class
-
+        
 def test_vacc_person_instantiation():
+    
     person = Person(1, True)
-
     assert person._id == 1
     assert person.is_alive is True
     assert person.is_vaccinated is True
-    assert person.infection is None
+    assert person.virus is None
+
 
 def test_not_vacc_person_instantiation():
     person = Person(2, False)
-
     assert person._id == 2
     assert person.is_alive is True
     assert person.is_vaccinated is False
-    assert person.infection is None
+    assert person.virus is None
+
 
 def test_sick_person_instantiation():
+    virus = Virus("Dysentery", 0.7, 0.2)
     person = Person(3, False, virus)
 
     assert person._id == 3
     assert person.is_alive is True
     assert person.is_vaccinated is False
-    assert person.infection == virus
+    assert person.virus is virus
+
 
 def test_did_survive_infection():
+    virus = Virus("Dysentery", 0.7, 0.2)
     person = Person(4, False, virus)
-    survived = person.did_survive_infection()
+    
+    assert person._id == 4
+    assert person.is_alive is True
+    assert person.is_vaccinated is False
+    assert person.virus is virus
 
+
+    survived = person.did_survive_infection()
+    
     if survived:
         assert person.is_alive is True
-        assert person._id == 4
-        assert person.is_vaccinated == True
-        assert person.infection == None
-        print('Survived: True')
+        assert person.natural_immunity is True
+
     else:
         assert person.is_alive is False
-        assert person._id == 4
-        assert person.is_vaccinated == False
-        assert person.infection == virus
-        print('Surived: False')
-
-if __name__ == '__main__':
-    virus = Virus('Ebola', .8, .3)
-
-    test_vacc_person_instantiation()
-    test_not_vacc_person_instantiation()
-    test_sick_person_instantiation()
-    test_did_survive_infection()
     
+
+# print(person.did_survive_infection())
+# print(person.did_survive_infection())
+# print(person.did_survive_infection())
+
+
+
+# print(test_vacc_person_instantiation())
+# print(test_not_vacc_person_instantiation())
+# print(test_sick_person_instantiation())
+
+# print(f'test: {test_sick_person_instantiation()}')
