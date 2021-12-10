cfae5780be1e98bac6ec5925aa71575d23471535
ArnavPag <arnav.pagidyala@students.dominican.edu>
2021-12-09 17:11:28 -0800

    test and check.py updated

---
+++ check.py
@@ -0,0 +1,10 @@
+def check(prompt='', type=int):
+    Incorrect = True
+    while Incorrect:
+        try:
+            user_input = type(input(prompt))
+        except ValueError:
+            print(f'Invalid! --- Should be {type}.')
+            continue
+        else:
+            return 
--- simulation_test.py
+++ simulation_test.py
@@ -1,1 +1,29 @@
-#file should be created that allows for testing the simulation.
+
+   
+from simulation import Simulation
+from virus import Virus
+from check import check
+
+print('\n\nWelcome to the Herd Immunity Simulatiom!')
+print('- - - - - - - - - - - - - - - - - - - - - - -  - - - - - - -')
+print('You will be asked for data to input into your simulation.')
+print('After running the simulation, you can access data in the output files. Enjoy!')
+print('- - - - - - - - - - - - - - - - - - - - - - -  - - - - - - -')
+
+
+virus_name = check('What is the virus name?  ', str)
+repro_rate = check('What is the reproduction rate?  ')/100
+mortality_rate = check('What is the mortality rate?  ')/100
+population_size = check('What is the population size?  ')
+vacc_percentage = check('What percentage of the population is vaccinated?  ')/100
+initial_infected = check('How many people are initially infected?  ')
+average_interactions = check('How many interactions should each infected person have?  ')
+
+
+virus = Virus(virus_name, repro_rate, mortality_rate)
+
+
+sim = Simulation (virus, population_size, vacc_percentage, initial_infected, average_interactions)
+
+
+sim.run()
